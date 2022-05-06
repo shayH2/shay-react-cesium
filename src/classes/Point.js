30d5d9bd8dfe8424dec03b920e1cbb308ed0e9f7
SH <shay.he3@gmail.com>
2022-05-06 18:59:20 +0300

    check commit

--- src/classes/Point.js
+++ src/classes/Point.js
@@ -1,36 +1,34 @@
 'use strict';
 
 export default class MyPoint {
-    constructor(x, y, z, coordsType, idx) {
-        this.coordX = x;
-        this.coordY = y;
-        this.z = z;
-        this.coordsType = coordsType;
-        this.index = idx;
-        this.distance = null;
-    }
+  constructor(x, y, z, coordsType, idx) {
+    this.coordX = x;
+    this.coordY = y;
+    this.z = z;
+    this.coordsType = coordsType;
+    this.index = idx;
+    this.distance = null;
+  }
 
-    get x() {
-        return this.coordX;
-    }
+  get x() {
+    return this.coordX;
+  }
 
-    set x(val) {
-        this.coordX = val;
-    }
+  set x(val) {
+    this.coordX = val;
+  }
 
-    get y() {
-        return this.coordY;
-    }
+  get y() {
+    return this.coordY;
+  }
 
-    set y(val) {
-        this.coordY = val;
-    }
+  set y(val) {
+    this.coordY = val;
+  }
 
-    getCoord = (index) => {
-        if (index === 1)
-            return this.coordX;
+  getCoord = (index) => {
+    if (index === 1) return this.coordX;
 
-        if (index === 2)
-            return this.coordY;
-    }
+    if (index === 2) return this.coordY;
+  };
 }
